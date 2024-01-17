4c16301fe54cb515312a10614bec613ad4385c2c
Dinesh V <dinesh.v@adlinktech.com>
2024-01-17 17:07:11 +0530

    Correct ADlink layer path

--- conf/adlink-conf/adlink-rb5n-setup-release.sh
+++ conf/adlink-conf/adlink-rb5n-setup-release.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-#TOKEN_PRE="ghp_"
-#TOKEN_POST="DiFPUbMC2PjG06sFvpuDrsBdJAH5bO0bSQd9"
 
 CWD=$(pwd)
 PROGNAME="$CWD/setup-environment"
@@ -9,12 +7,10 @@ PROGNAME="$CWD/setup-environment"
 
 MACHINE=$MACHINE DISTRO=$DISTRO source $PROGNAME $@
 
-if [ -f ../layers/meta-adlink-qualcomm/conf/adlink-conf/$MACHINE/bblayers.conf.append ]; then
-	cat ../layers/meta-adlink-qualcomm/conf/adlink-conf/$MACHINE/bblayers.conf.append >> ./conf/bblayers.conf
+if [ -f ../meta-adlink-qualcomm/conf/adlink-conf/$MACHINE/bblayers.conf.append ]; then
+	cat ../meta-adlink-qualcomm/conf/adlink-conf/$MACHINE/bblayers.conf.append >> ./conf/bblayers.conf
 fi
-if [ -f ../layers/meta-adlink-qualcomm/conf/adlink-conf/$MACHINE/local.conf.append ]; then
-	cat ../layers/meta-adlink-qualcomm/conf/adlink-conf/$MACHINE/local.conf.append >> ./conf/local.conf
+if [ -f ../meta-adlink-qualcomm/conf/adlink-conf/$MACHINE/local.conf.append ]; then
+	cat ../meta-adlink-qualcomm/conf/adlink-conf/$MACHINE/local.conf.append >> ./conf/local.conf
 fi
 
-#echo "PA_USER ?= \"adlink-guest\"" >> ./conf/local.conf
-#echo "PA_TOKEN ?= \""${TOKEN_PRE}${TOKEN_POST}"\"" >> ./conf/local.conf
