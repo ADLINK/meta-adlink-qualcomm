78192edba3a4b2d21446e9b80d059de302034a05
deepak.s <deepak.s@adlinktech.com>
2024-06-03 16:25:06 +0530

    LEC-RB5 machine,update BBpath and script file

--- conf/adlink-conf/adlink-rb5n-setup-release.sh
+++ conf/adlink-conf/adlink-rb5n-setup-release.sh
@@ -8,9 +8,9 @@ PROGNAME="$CWD/setup-environment"
 MACHINE=$MACHINE DISTRO=$DISTRO source $PROGNAME $@
 
 if [ -f ../meta-adlink-qualcomm/conf/adlink-conf/$MACHINE/bblayers.conf.append ]; then
-	cat ../meta-adlink-qualcomm/conf/adlink-conf/$MACHINE/bblayers.conf.append >> ./conf/bblayers.conf
+	grep "meta-adlink-qualcomm" ./conf/bblayers.conf  > /dev/null 2>&1 || cat ../meta-adlink-qualcomm/conf/adlink-conf/$MACHINE/bblayers.conf.append >> ./conf/bblayers.conf 
 fi
 if [ -f ../meta-adlink-qualcomm/conf/adlink-conf/$MACHINE/local.conf.append ]; then
-	cat ../meta-adlink-qualcomm/conf/adlink-conf/$MACHINE/local.conf.append >> ./conf/local.conf
+	grep "adlinkweston" ./conf/local.conf > /dev/null 2>&1 || cat ../meta-adlink-qualcomm/conf/adlink-conf/$MACHINE/local.conf.append >> ./conf/local.conf
 fi
 
